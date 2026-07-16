68c91688f967d957c3be1b3e08e97f6e330d5f71
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-07-16 23:11:55 +0530

    Fix declarative pipeline syntax

--- vars/testPipeline.groovy
+++ vars/testPipeline.groovy
@@ -1,36 +1,36 @@
-def call(Map configMap){
-    pipeline {
-        agent {
-            node {
-                label 'roboshop' 
-            } 
-        }
-        stages {
-            stage('testing') {
-                steps {
-                    script{
-                        sh """
-                            echo "project: ${configMap.project}"
-                            """   
-                        }
-                   }   
-                }
-            }
-        }
+// def call(Map configMap){
+//     pipeline {
+//         agent {
+//             node {
+//                 label 'roboshop' 
+//             } 
+//         }
+//         stages {
+//             stage('testing') {
+//                 steps {
+//                     script{
+//                         sh """
+//                             echo "project: ${configMap.project}"
+//                             """   
+//                         }
+//                    }   
+//                 }
+//             }
+//         }
         
             
-        // post build
-        post { 
-            always { 
-                echo 'I will always say Hello again!'
-                cleanWs()
-            }
-            success {
-                echo "pipeline success"
-            }
-            failure {
-                echo "pipeline failure"
-            }
-        }
-    }
-}
+//         // post build
+//         post { 
+//             always { 
+//                 echo 'I will always say Hello again!'
+//                 cleanWs()
+//             }
+//             success {
+//                 echo "pipeline success"
+//             }
+//             failure {
+//                 echo "pipeline failure"
+//             }
+//         }
+//     }
+// }
