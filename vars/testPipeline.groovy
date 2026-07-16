ebbb3c6f76098772a80bf086e8ca42e8ae9111e0
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-07-16 20:47:12 +0530

    pipeline

--- vars/testPipeline.groovy
+++ vars/testPipeline.groovy
@@ -1,35 +1,35 @@
 def call(Map configMap){
     pipeline
-    agent {
-        node {
-            label 'roboshop' 
-        } 
-    }
-    stages {
-        stage('Testing') {
-            steps {
-                script{
-                   sh """
-                       echo "project: ${configMap.project}"
-                    """   
+        agent {
+            node {
+                label 'roboshop' 
+            } 
+        }
+        stages {
+            stage('Testing') {
+                steps {
+                    script{
+                    sh """
+                        echo "project: ${configMap.project}"
+                        """   
+                    }
+                    
                 }
-                
+            }
+        }
+        
+            
+        // post build
+        post { 
+            always { 
+                echo 'I will always say Hello again!'
+                cleanWs()
+            }
+            success {
+                echo "pipeline success"
+            }
+            failure {
+                echo "pipeline failure"
             }
         }
     }
-    
-        
-      // post build
-    post { 
-        always { 
-            echo 'I will always say Hello again!'
-            cleanWs()
-        }
-        success {
-            echo "pipeline success"
-        }
-        failure {
-            echo "pipeline failure"
-        }
-    }
-}
